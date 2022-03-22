497190d0ec33594dd3a14549b5b6513006a50255
sanjay <ssah6694@gmail.com>
2022-03-22 12:38:16 +0530

    [sanjay]Added: Refactor2 Maximum value with genenric Class file

---
+++ GenericMaximum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactor2_UsingGenericClass
+{
+    public class GenericMaximum<T> where T:IComparable
+    {
+        public T FirstValue;
+        public T SecondValue; 
+        public T ThirdValue;
+
+        public GenericMaximum(T firstValue, T secondValue , T thirdValue )
+        {
+            this.FirstValue = firstValue;
+            this.SecondValue = secondValue;
+            this.ThirdValue = thirdValue;
+
+        }
+
+        public static T MaximumValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if(firstValue.CompareTo(secondValue)>0 && firstValue.CompareTo(thirdValue)>0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0 )
+            {
+                return firstValue;
+            }
+            if(secondValue.CompareTo(firstValue)>0 && secondValue.CompareTo(thirdValue)>0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0 )
+            {
+                return secondValue;
+            }
+            if(thirdValue.CompareTo(firstValue)>0 && thirdValue.CompareTo(secondValue)>0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0 )
+            {
+                return thirdValue;
+            }
+            return default;
+        }
+         public  T MaxMethod()
+         {
+            T max = GenericMaximum<T>.MaximumValue(this.FirstValue, this.SecondValue, this.ThirdValue);
+            return max;
+            
+         }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Refactor2_UsingGenericClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericMaximum<int> obj = new GenericMaximum<int>(23,45,10);
+            int result=obj.MaxMethod();
+            Console.WriteLine("Maximum value of integer is= "+result);
+
+            GenericMaximum<double> obj1 = new GenericMaximum<double>(45.19, 45.76, 99.99);
+            double result1=obj1.MaxMethod();
+            Console.WriteLine("Maximum value of double is= "+result1);
+
+            GenericMaximum<string> obj2 = new GenericMaximum<string>("454", "657", "968");
+            string result2=obj2.MaxMethod();
+            Console.WriteLine("Maximum value of string is= "+result2);
+        }
+    }
+}
