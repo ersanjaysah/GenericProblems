65693361bc74113f2dbf68fa9fd2636d29df4663
sanjay <ssah6694@gmail.com>
2022-03-22 12:47:15 +0530

    [sanjay]Added: UC5 maximum with GenericMax file

---
+++ GneericMaximum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4_UsingOption_andSorting
+{
+    public class GenericMaximum<T> where T:IComparable
+    {
+        public T[] Value;       
+
+        public GenericMaximum(T[] value)   //constructor
+        {
+            this.Value = value;
+        }
+
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+           
+        }
+
+        public T MaxValueFromArray(T[] values)
+        {
+            T[] sorted_values = Sort(values);
+            return sorted_values[sorted_values.Length - 1];
+        }
+
+        public T MaxMethodFromArray()
+        {
+            var max = MaxValueFromArray(this.Value);
+            return max;
+        }
+
+        public void PrintMaxValue()
+        {
+            var max=MaxValueFromArray(this.Value);
+            Console.WriteLine("Maximum Value is = "+max);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UC4_UsingOption_andSorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find maximum number");
+
+            int[] intArray = { 3, 4, 7, 9, 2 };
+            GenericMaximum<int> obj = new GenericMaximum<int>(intArray);
+            obj.PrintMaxValue();
+            
+
+
+        }
+    }
+}
