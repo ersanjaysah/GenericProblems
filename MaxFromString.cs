07334701c1f08857d0c7e672acee37295a495da2
sanjay <ssah6694@gmail.com>
2022-03-22 12:30:17 +0530

    [sanjay]Added: UC3 Maximum value of string file

---
+++ MaxFromString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC3_FindMax_WithString
+{
+    public class MaxFromString
+    {
+        public static string Maximum_String(string firstNumber, string secondNumber, string thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0 ||
+                firstNumber.CompareTo(secondNumber) >= 0 && firstNumber.CompareTo(thirdNumber) > 0 ||
+                firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) >= 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0 ||
+                secondNumber.CompareTo(firstNumber) >= 0 && secondNumber.CompareTo(thirdNumber) > 0 ||
+                secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) >= 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0 ||
+                thirdNumber.CompareTo(firstNumber) >= 0 && thirdNumber.CompareTo(secondNumber) > 0 ||
+                thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) >= 0)
+            {
+                return thirdNumber;
+            }
+            return firstNumber;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UC3_FindMax_WithString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum From string");
+
+            Console.WriteLine("Enter First string Value");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("Enter Second string Value");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine("Enter Third string Value");
+            string num3 = Console.ReadLine();
+
+
+           string result=MaxFromString.Maximum_String(num1,num2,num3);
+            Console.WriteLine("The Maximum Value is = "+result);
+        }
+    }
+}
