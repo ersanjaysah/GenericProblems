bc0f0b6fa0f1cf95362d3dbda897deff49cd17eb
sanjay <ssah6694@gmail.com>
2022-03-22 12:20:49 +0530

    [sanjay]Added: UC1 Maximum value of integer file

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MaximumFrom_Integers
+{
+    internal class MaximumNumberCheck
+    {
+        public static int MaximumInteger(int firstValue,int secondValue,int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue)>0 && firstValue.CompareTo(thirdValue)>0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue)>0 && secondValue.CompareTo(thirdValue)>0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0 )
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue)>0 && thirdValue.CompareTo(secondValue)>0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0 )
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+
+
+
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumFrom_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the find Maximum Program");
+
+            Console.WriteLine("enter First number");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter Second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter Third number");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int max=MaximumNumberCheck.MaximumInteger(num1, num2, num3);
+            Console.WriteLine("The Maximum value is= "+max);
+        }
+    }
+}
