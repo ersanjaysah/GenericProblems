f657376070cf8e4f013391997929bc8c1abd2e3d
sanjay <ssah6694@gmail.com>
2022-03-22 12:42:13 +0530

    [sanjay]Added: UC4 maximum with more the 3 parameters file

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Uc4MaxMethod_WithParameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] doubleArray = new double[5];
+            Console.WriteLine("enter array elements");
+            for (int i = 0; i < doubleArray.Length; i++)
+            {
+                doubleArray[i] = double.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(doubleArray);       // sorting an array
+            int a = doubleArray.Length - 1;
+            Console.WriteLine("the maximum number is= " + doubleArray[a]);
+            Console.WriteLine("sorted list of an Array is");
+            foreach (double i in doubleArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
