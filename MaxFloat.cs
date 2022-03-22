dfb74c09c871298f855ca06c2fb598fb11a93aec
sanjay <ssah6694@gmail.com>
2022-03-22 12:26:54 +0530

    [sanjay]Added: UC2 Maximum value of Float file

---
+++ MaxFloat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_FindMaxNumberBy_Double
+{
+    public class MaxFloat
+    {
+        public static float Maximum_Float(float firstNumber,float secondNumber, float thirdNumber)
+        {
+            if(firstNumber.CompareTo(secondNumber)>0 && firstNumber.CompareTo(thirdNumber)>0 ||
+                firstNumber.CompareTo(secondNumber) >= 0 && firstNumber.CompareTo(thirdNumber) > 0 ||
+                firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) >= 0 )
+            {
+                return firstNumber;
+            }
+            if(secondNumber.CompareTo(firstNumber)>0 && secondNumber.CompareTo(thirdNumber)>0 ||
+                secondNumber.CompareTo(firstNumber) >= 0 && secondNumber.CompareTo(thirdNumber) > 0 ||
+                secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) >= 0 )
+            {
+                return secondNumber;
+            }
+            if(thirdNumber.CompareTo(firstNumber)>0 && thirdNumber.CompareTo(secondNumber)>0 ||
+                thirdNumber.CompareTo(firstNumber) >= 0 && thirdNumber.CompareTo(secondNumber) > 0 ||
+                thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) >= 0 )
+            {
+                return thirdNumber;
+            }
+            return firstNumber;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UC2_FindMaxNumberBy_Double
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter first number of Float Value");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter Second number of Float Value");
+            float num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter third number of folat Value");
+            float num3 = float.Parse(Console.ReadLine());
+
+
+            float reault=MaxFloat.Maximum_Float(num1,num2,num3);
+            Console.WriteLine("so maximum number is= "+reault);
+        }
+    }
+}
